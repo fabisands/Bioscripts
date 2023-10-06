675138382701000ef92308773daa2b47180afba7
fabisands <f.arenas0294@gmail.com>
2023-10-06 03:46:17 -0500

    :sparkles: :hammer:

--- Amino-code.py
+++ Amino-code.py
@@ -1,6 +1,34 @@
-# Take the DNA code 5'-TTCCTCCTTGGCCGCGTGCTTCGTCTG-3'
-# Split it into RNA and discover which amino acids it makes
-# Look for one letter code for every amino acid and discover the phrase
+genetic_code = {
+    'TTT': 'F', 'TTC': 'F', 'TTA': 'L', 'TTG': 'L',
+    'CTT': 'L', 'CTC': 'L', 'CTA': 'L', 'CTG': 'L',
+    'ATT': 'I', 'ATC': 'I', 'ATA': 'I', 'ATG': 'M',
+    'GTT': 'V', 'GTC': 'V', 'GTA': 'V', 'GTG': 'V',
+    'TCT': 'S', 'TCC': 'S', 'TCA': 'S', 'TCG': 'S',
+    'CCT': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+    'ACT': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+    'GCT': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+    'TAT': 'Y', 'TAC': 'Y', 'TAA': '*', 'TAG': '*',
+    'CAT': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
+    'AAT': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',
+    'GAT': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+    'TGT': 'C', 'TGC': 'C', 'TGA': '*', 'TGG': 'W',
+    'CGT': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
+    'AGT': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',
+    'GGT': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G'
+}
 
-DNA_CODE = "5'-TTCCTCCTTGGCCGCGTGCTTCGTCTG-3'"
+# Function for translation of a DNA sequence into an aminoacid sequence
+def translate_dna_to_aa(dna_sequence):
+    dna_sequence = dna_sequence.upper()
+    aa_sequence = ""
+    
+    for i in range(0, len(dna_sequence), 3):
+        codon = dna_sequence[i:i+3]
+        aminoacid = genetic_code.get(codon, 'X')  # 'X' is for an unknown codon
+        aa_sequence += aminoacid
+        
+    return aa_sequence
 
+dna_sequence = input("Write a series of letters of nitrogenous bases (CGTA): ")
+aa_sequence = translate_dna_to_aa(dna_sequence)
+print("Translated aminoacid sequence:", aa_sequence)
